25761dadb206f7fe52cd07666360218294d89093
Salihk1git <msalihk1@gmail.com>
2022-08-10 15:35:28 +0530

    updating files

--- app.py
+++ app.py
@@ -38,12 +38,12 @@ def predict():
     print(f"The  Predicted values is :{prediction[0][1]}")
 
     if prediction[0][1] >= 0.5:
-        valPred = round(prediction[0][1],3)
-        print(f"The Round val {valPred*100}%")
-        return render_template('result.html',pred=f'You have a chance of having diabetes.\n\nProbability of you being a diabetic is {valPred*100}%.\n\nAdvice : Exercise Regularly')
+        
+        return render_template('result.html',pred=f'The chance of employee getting promoted')
     else:
-        valPred = round(prediction[0][0],3)
-        return render_template('result.html',pred=f'Congratulations!!!, You are in a Safe Zone.\n\n Probability of you being a non-diabetic is {valPred*100}%.\n\n Advice : Exercise Regularly and maintain like this..!')
+        
+        return render_template('result.html',pred=f'There is no chance of employee  getting promoted.')
 if __name__ == "__main__":
     app.run(debug=True)
+
           
