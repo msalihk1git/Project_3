87709fbe2e42e0e0cf804efc6591517a05c3d332
Salihk1git <msalihk1@gmail.com>
2022-10-12 16:00:22 +0530

    update files

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import joblib
 app = Flask(__name__)
 
 
-# #model = pickle.load(open("model.pkl","rb"))
+#model = pickle.load(open("model.pkl","rb"))
 model = joblib.load("model.pkl")
-# #scale = pickle.load(open("scale.pkl","rb"))
+#scale = pickle.load(open("scale.pkl","rb"))
 scale = joblib.load("scale.pkl")
 le_dept=joblib.load('le_dept.pkl')
 le_edu=joblib.load('le_edu.pkl')
@@ -26,25 +26,25 @@ def landingPage():
 @app.route("/predict",methods=['POST'])
 def predict():
     
-    Department = request.form['Department']
-    Department = le_dept.transform(Department)
-    region = request.form['Region']
-    region = le_reg.transform(region)
+    Department = request.form['1']
+    Department = le_dept.transform([Department])[0]
+    region = request.form['2']
+    region = le_reg.transform([region])[0]
     # print(Region)
     # Region = region_label.tranform(Region)
-    Education = request.form['Education']
-    Education = le_edu.transform(Education)
-    Gender = request.form['Gender']
-    Gender = le_gen.transform(Gender)
-    recruitment_channel = request.form['Recruitment Channel']
-    recruitment_channel = le_rech.transform(recruitment_channel)
-    no_of_trainings = request.form['Number of Trainings']
-    age = request.form['age']
-    previous_year_rating = request.form['previous_year_rating']
-    length_of_service = request.form['length_of_service']
-    KPIs_met_above_80_percent = request.form['KPIs_met_above_80_percent']
-    awards_won = request.form['awards_won']
-    avg_training_score = request.form['avg_training_score']
+    Education = request.form['3']
+    Education = le_edu.transform([Education])[0]
+    Gender = request.form['4']
+    Gender = le_gen.transform([Gender])[0]
+    recruitment_channel = request.form['5']
+    recruitment_channel = le_rech.transform([recruitment_channel])[0]
+    no_of_trainings = request.form['6']
+    age = request.form['7']
+    previous_year_rating = request.form['8']
+    length_of_service = request.form['9']
+    KPIs_met_above_80_percent = request.form['10']
+    awards_won = request.form['11']
+    avg_training_score = request.form['12']
 
     # return "true"
 
@@ -60,11 +60,12 @@ def predict():
     if prediction[0][1] >= 0.5:
         valPred = round(prediction[0][1],3)
         print(f"The Round val {valPred*100}%")
-        return render_template('result.html',pred=f'Probability of not being Promoted is {valPred*100}%.')
+        return render_template('result.html',pred=f'Probability of  being Promoted is {valPred*100}%.')
     else:
         valPred = round(prediction[0][0],3)
 
-        return render_template('result.html',pred=f'probability of being Promoted is {valPred*100}%.')
+        return render_template('result.html',pred=f'probability of being  not Promoted is {valPred*100}%.')
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
